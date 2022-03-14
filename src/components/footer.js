1775479b4243470b31301ef1e07b455eae8ec3aa
Tavaris Thompson <varis93@yahoo.com>
2022-03-14 15:08:32 -0400

    working

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ function Footer() {
             <div className='img-link'>
                 <a href="https://github.com/varis1993" target="_blank"><img src="https://img.icons8.com/color/48/000000/github-2.png" alt="Github" className="icon"/></a>
                 <a href="https://www.linkedin.com/feed/?trk=nav_logo" target="_blank"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
-                <a href="https://twitter.com/priyaravi231" target="_blank"><img src="https://img.icons8.com/color/48/000000/twitter.png" alt="Twitter" className="icon"/></a>
+                <a href="https://mail.google.com/mail/u/0/#inbox" target="_blank"><img src="https://img.icons8.com/color/48/000000/twitter.png" alt="Twitter" className="icon"/></a>
             </div>
 
         </div>
